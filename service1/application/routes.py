d2eaab45ca8d0ea89fd0b9e71aeec24aaedfb195
Tobi Jackson <jtsproduction@live.com>
2020-08-19 15:30:43 +0100

    ok

--- service1/application/routes.py
+++ service1/application/routes.py
@@ -6,10 +6,10 @@ import requests
 def home():
     return render_template('home.html', title='Home')
 
-@app.route('/generate_animal',methods=['GET','POST'])
+@app.route('/get_animal',methods=['GET','POST'])
 def generate_animal():
-    animal = requests.get('http://localhost:5001/get/animal')
-    sound = requests.post('http://localhost:5001/get/sound', data=animal.text)
+    animal = requests.get('http://service2:5001/animal')
+    sound = requests.post('http://service2:5001/sound', data=animal.text)
     return render_template('animals.html', title='Animal', animal = animal.text, sound = sound.text)
 
 
